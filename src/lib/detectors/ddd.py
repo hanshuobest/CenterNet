08587cb7f168c642422c951d23f790c6209ba951
shuo.han <shuo.han@bianlifeng.com>
2020-03-25 16:41:55 +0800

    修改ddd.py，使其落图到磁盘

--- src/lib/detectors/ddd.py
+++ src/lib/detectors/ddd.py
@@ -103,4 +103,5 @@ class DddDetector(BaseDetector):
       center_thresh=self.opt.vis_thresh, img_id='add_pred')
     debugger.add_bird_view(
       results, center_thresh=self.opt.vis_thresh, img_id='bird_pred')
-    debugger.show_all_imgs(pause=self.pause)
+    # debugger.show_all_imgs(pause=self.pause)
+    debugger.save_all_imgs(path='/home/han/git_project/detection/CenterNet/output_result', genID=True)
